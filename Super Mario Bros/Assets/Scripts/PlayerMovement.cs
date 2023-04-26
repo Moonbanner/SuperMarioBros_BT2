ddfb5faceee19cb5d58441b93bbfa0522686dd55
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-04-26 13:54:27 +0700

    Horizontal Movement
    Created Mario prefab and PlayerMovement script, also add that script as a component to Mario prefab/GameObject
    The Mario sprite can now float left and right

---
+++ Super Mario Bros/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody; //same name as a very old, obsolete property, hence the 'new'
+
+    private Vector2 velocity;
+    private float inputAxis;
+
+    public float moveSpeed = 8.0f;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position);
+
+    }
+}
