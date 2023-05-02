e28efb3a35a2975d4e9927244988810bc764c12f
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-05-03 01:57:31 +0700

    Jumping and Gravity
    Implemented jumping for Mario with the Space button
    Holding down space will eventually make Mario jump to the max height (5 currently), while tapping Space wont make Mario reach the max height.
    Functions and variables defined to make jumping works:
    +Raycast in the extensions script (for later use in other scripts)
    + gravity
    + grounded
    + falling
    ...

--- Super Mario Bros/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 8.0f;
     public float maxJumpHeight = 5.0f;
     public float maxJumpTime = 1.0f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
 
     private void Awake()
     {
@@ -21,12 +26,42 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HorizontalMovement();
+
+        grounded = rigidbody.Raycast(Vector2.down);
+
+        if (grounded)
+        {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void HorizontalMovement()
     {
         inputAxis = Input.GetAxis("Horizontal");
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void GroundedMovement()
+    {
+        velocity.y = Mathf.Max(velocity.y, 0f); //prevents y velocity from building up too much while Mario is grounded
+        jumping = velocity.y > 0f;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");     //Handles the gravity to make Mario jump higher
+        float multiplier = falling ? 2f : 1f;                           //if the player keep holding down the jump button
+
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f); //Terminal velocity, prevents Mario from falling TOO FAST
     }
 
     private void FixedUpdate()
