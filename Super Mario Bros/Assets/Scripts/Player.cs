f6da5d606abb86708170217d463c97a4516a4687
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-05-05 20:06:05 +0700

    Halfway DeathAnimation implementation
    player state not completed

---
+++ Super Mario Bros/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerSpriteRenderer smallRenderer;
+    public PlayerSpriteRenderer bigRenderer;
+
+    private DeathAnimation deathAnimation;
+    public void Hit()
+    {
+        
+    }
+}
