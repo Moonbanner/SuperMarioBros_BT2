744898fc83baa125d8ab99ed33ba2c24fd31dd18
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-05-13 20:56:58 +0700

    Underground Camera
    Modified the SideScrolling so that the camera changes its y position when Mario changes his - enters and exits pipes.
    The new SetUnderground() function of 'SideScrolling' script is then used in 'Pipe' script - inside Enter()

--- Super Mario Bros/Assets/Scripts/Pipe.cs
+++ Super Mario Bros/Assets/Scripts/Pipe.cs
@@ -32,6 +32,9 @@ public class Pipe : MonoBehaviour
         yield return Move(player, enteredPosition, enteredScale);
         yield return new WaitForSeconds(1f);
 
+        bool isUnderground = connection.position.y < 0f;
+        Camera.main.GetComponent<SideScrolling>().SetUnderground(isUnderground);
+
         if (exitDirection != Vector3.zero)
         {
             player.position = connection.position - exitDirection;
--- Super Mario Bros/Assets/Scripts/SideScrolling.cs
+++ Super Mario Bros/Assets/Scripts/SideScrolling.cs
@@ -4,6 +4,9 @@ public class SideScrolling : MonoBehaviour
 {
     private Transform player;
 
+    public float height = 7f; //6.5f in tutorial video (part 5)
+    public float undergroundHeight = -9f;
+
     private void Awake()
     {
         player = GameObject.FindWithTag("Player").transform; //Not recommended if game has more than 1 player, if so, set public Transform player, then drag player in in Unity Editor
@@ -14,6 +17,14 @@ public class SideScrolling : MonoBehaviour
     {
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+
+    //passed variable is called 'underground' in tutorial video (part 5)
+    public void SetUnderground(bool isUnderground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = isUnderground ? undergroundHeight : height;
         transform.position = cameraPosition;
     }
 }
